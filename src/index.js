b6598db89760eade9133ff7fe139658839ec9689
Trần Hữu Tài <99662952+Tran-Huu-Tai-12-04-23@users.noreply.github.com>
2023-09-18 16:43:01 +0700

    done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-require("dotenv").config();
-const express = require("express");
-const cors = require("cors");
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
 const app = express();
-const bodyParser = require("body-parser");
-const connectDb = require("./configDb");
+const bodyParser = require('body-parser');
+const connectDb = require('./configDb');
 const path = require('path');
 
-const staticFolderPath = path.join(__dirname); 
+const staticFolderPath = path.join(__dirname);
 app.use(express.static(staticFolderPath));
 
 app.use(express.static('public'));
@@ -15,10 +15,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 connectDb();
 const port = process.env.PORT || 3000;
-const applyRoute = require("./Route/index");
+const applyRoute = require('./Route/index');
 
 applyRoute(app);
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on port ${port}`);
 });
