e556527c5275e1bfa677701ff657851b8e5be69c
Trần Hữu Tài <99662952+Tran-Huu-Tai-12-04-23@users.noreply.github.com>
2023-10-02 14:09:29 +0700

    config deploy

--- src/Route/index.js
+++ src/Route/index.js
@@ -1,17 +1,24 @@
-const user = require("./user");
-const project = require("./project");
-const task = require("./task");
-const note = require("./note");
-const column = require("./column");
-const link = require("./link");
+const user = require('./user');
+const project = require('./project');
+const task = require('./task');
+const note = require('./note');
+const column = require('./column');
+const link = require('./link');
+const router = require('./user');
 
 const applyRoute = (app) => {
-  app.use("/user", user);
-  app.use("/project", project);
-  app.use("/task", task);
-  app.use("/note", note);
-  app.use("/col", column);
-  app.use("/link", link);
+    app.use(
+        '/',
+        router.get('/', (req, res) => {
+            return res.json('Start with api');
+        }),
+    );
+    app.use('/user', user);
+    app.use('/project', project);
+    app.use('/task', task);
+    app.use('/note', note);
+    app.use('/col', column);
+    app.use('/link', link);
 };
 
 module.exports = applyRoute;
